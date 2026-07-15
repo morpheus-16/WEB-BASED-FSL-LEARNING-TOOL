db553c747725ba43f8473feb6753f48a27cbaa95
marian_marchan <142994591+mrxiann@users.noreply.github.com>
2026-07-15 10:02:26 +0800

    dependencies

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\dexte\Documents\GitHub\WEB-BASED-FSL-LEARNING-TOOL\backend\venv
+set VIRTUAL_ENV=C:\web based fsl\WEB-BASED-FSL-LEARNING-TOOL\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
